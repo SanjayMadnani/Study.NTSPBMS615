e096fee727c825055c5832487e01e58e4fcc2242
SanjayMadnani <sanjay.madnani@outlook.com>
2021-09-19 05:58:15 +0530

    Committing setter injection with constructor code to verify that construction inject will occur first as object creation occurs before method (set method) call.

--- IOCProject02-ConstructorInjection/src/main/java/com/nt/beans/WishMessageGenerator.java
+++ IOCProject02-ConstructorInjection/src/main/java/com/nt/beans/WishMessageGenerator.java
@@ -6,19 +6,29 @@ public class WishMessageGenerator {
 	
 	Date date;
 
+	//constructor-arg name="date"
+	//constructor can be private, still constructor injection will work
 	public WishMessageGenerator(Date date) {
 		System.out.println("WishMessageGenerator.1-param constructor");
 		this.date = date;
 	}
 	
+//	property name="date"
+	//should be public then only setter injection will work
+	public void setDate(Date date) {
+		System.out.println("WishMessageGenerator.setDate()");
+		this.date = date;
+	}
+
+
 	public String generateMessage(String name) {
 		System.out.println("WishMessageGenerator.generateMessage()");
 		int hour = date.getHours();
 		
-		if(hour < 12) return "Good Morning " + name;
-		else if(hour < 16) return "Good Afternoon " + name;
-		else if(hour < 20) return "Good Evening " + name;
-		else return "Good Night " + name;
+		if(hour < 12) return "Good Morning " + name + ", Date: " + date;
+		else if(hour < 16) return "Good Afternoon " + name + ", Date: " + date;
+		else if(hour < 20) return "Good Evening " + name + ", Date: " + date;
+		else return "Good Night " + name + ", Date: " + date;
 	}
 
 }
