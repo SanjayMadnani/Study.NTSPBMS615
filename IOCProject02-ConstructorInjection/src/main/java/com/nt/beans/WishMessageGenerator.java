299265773dd96f83bde87f95a310949ea83d0bc3
SanjayMadnani <sanjay.madnani@outlook.com>
2021-09-19 06:40:31 +0530

    testing of new object creation on each bean id. Also verified case sensitivity of bean name with setter injection and constructor injection.

--- IOCProject02-ConstructorInjection/src/main/java/com/nt/beans/WishMessageGenerator.java
+++ IOCProject02-ConstructorInjection/src/main/java/com/nt/beans/WishMessageGenerator.java
@@ -3,32 +3,40 @@ package com.nt.beans;
 import java.util.Date;
 
 public class WishMessageGenerator {
-	
+
 	Date date;
 
-	//constructor-arg name="date"
+	//constructor-arg name="date" must match with @param date of this constructor
 	//constructor can be private, still constructor injection will work
 	public WishMessageGenerator(Date date) {
 		System.out.println("WishMessageGenerator.1-param constructor");
 		this.date = date;
 	}
-	
-//	property name="date"
+
+	public WishMessageGenerator() {
+		System.out.println("WishMessageGenerator.0-Param Constructor");
+	}
+
+	//	property name="date" (or Date) must match with the set<Date> this method
+	// Note: property name="Date" does not match with the set<date> method (Incorrect)
 	//should be public then only setter injection will work
 	public void setDate(Date date) {
 		System.out.println("WishMessageGenerator.setDate()");
 		this.date = date;
 	}
 
-
 	public String generateMessage(String name) {
 		System.out.println("WishMessageGenerator.generateMessage()");
 		int hour = date.getHours();
-		
-		if(hour < 12) return "Good Morning " + name + ", Date: " + date;
-		else if(hour < 16) return "Good Afternoon " + name + ", Date: " + date;
-		else if(hour < 20) return "Good Evening " + name + ", Date: " + date;
-		else return "Good Night " + name + ", Date: " + date;
+
+		if (hour < 12)
+			return "Good Morning " + name + ", Date: " + date;
+		else if (hour < 16)
+			return "Good Afternoon " + name + ", Date: " + date;
+		else if (hour < 20)
+			return "Good Evening " + name + ", Date: " + date;
+		else
+			return "Good Night " + name + ", Date: " + date;
 	}
 
 }
