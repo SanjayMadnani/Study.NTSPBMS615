2b16a6aa64e963b1cddc21e5ea42c4f90d0a53c5
Sanjay Madnani <sanjay.madnani@outlook.com>
2021-09-28 00:20:58 +0530

    uses of ClassPathResource, DefaultListableBeanFactory and XmlBeanDefinitionReader.

--- IOCProject02-ConstructorInjection/src/main/java/com/nt/test/ConstructorInjectionTest.java
+++ IOCProject02-ConstructorInjection/src/main/java/com/nt/test/ConstructorInjectionTest.java
@@ -1,6 +1,9 @@
 package com.nt.test;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 import com.nt.beans.WishMessageGenerator;
@@ -8,10 +11,21 @@ import com.nt.beans.WishMessageGenerator;
 public class ConstructorInjectionTest {
 	
 	public static void main(String[] args) {
-		FileSystemResource resource = new FileSystemResource("src/main/java/com/nt/config/applicationContext.xml");
+//		FileSystemResource resource = new FileSystemResource("src/main/java/com/nt/config/applicationContext.xml");
+		ClassPathResource resource = new ClassPathResource("com/nt/config/applicationContext.xml");
 		XmlBeanFactory factory = new XmlBeanFactory(resource);
-		WishMessageGenerator messageGenerator = (WishMessageGenerator) factory.getBean("wmg");
+//		WishMessageGenerator messageGenerator = (WishMessageGenerator) factory.getBean("wmg");
+		WishMessageGenerator messageGenerator = factory.getBean("wmg", WishMessageGenerator.class);
 		System.out.println(messageGenerator.generateMessage("Sanjay"));
+		
+		System.out.println();
+		
+		DefaultListableBeanFactory fac = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(fac);
+		reader.loadBeanDefinitions("com/nt/config/applicationContext.xml");
+		WishMessageGenerator messageGenerator1 = fac.getBean("wmg", WishMessageGenerator.class);
+		System.out.println(messageGenerator1.generateMessage("Madnani"));
+		
 		
 		System.out.println();
 //		object for wmg already exist, hence no new object will be created
