70ecb32bb9b11351ea5533089fa6b948baee6700
Sanjay Madnani <sanjay.madnani@outlook.com>
2021-09-29 18:42:46 +0530

    Using properties file to make code 100% loosely coupled.

--- IOCProject05-StragegyDPWithFactoryDP-CoreJava/src/main/java/com/nt/factory/FlipkartFactory.java
+++ IOCProject05-StragegyDPWithFactoryDP-CoreJava/src/main/java/com/nt/factory/FlipkartFactory.java
@@ -1,5 +1,11 @@
 package com.nt.factory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.Properties;
+
 import com.nt.comp.BlueDart;
 import com.nt.comp.Courier;
 import com.nt.comp.DTDC;
@@ -7,7 +13,28 @@ import com.nt.comp.Flipkart;
 
 public class FlipkartFactory {
 	
-	public static Flipkart createFlipkart(String courierType) {
+	private static Properties properties;
+	
+	static {
+		try {
+			FileInputStream fis = new FileInputStream("src/main/java/com/nt/common/Info.porperties");
+			properties = new Properties();
+			properties.load(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Flipkart createFlipkart() throws Exception {
+		Flipkart flipkart = new Flipkart();
+		Class c = Class.forName(properties.getProperty("dependent.comp"));
+		Constructor cons[] = c.getDeclaredConstructors();
+		Courier courier = (Courier) cons[0].newInstance();
+		flipkart.setCourier(courier);
+		return flipkart;
+	}
+	
+	/*public static Flipkart createFlipkart(String courierType) {
 		Flipkart flipkart = new Flipkart();
 		Courier courier = null;
 		if("DTDC".equalsIgnoreCase(courierType)) {
@@ -19,6 +46,6 @@ public class FlipkartFactory {
 		}
 		flipkart.setCourier(courier);
 		return flipkart;
-	}
+	}*/
 
 }
--- IOCProject05-StragegyDPWithFactoryDP-CoreJava/src/main/java/com/nt/test/StrategyDPTest.java
+++ IOCProject05-StragegyDPWithFactoryDP-CoreJava/src/main/java/com/nt/test/StrategyDPTest.java
@@ -5,8 +5,9 @@ import com.nt.factory.FlipkartFactory;
 
 public class StrategyDPTest {
 	
-	public static void main(String[] args) {
-		Flipkart flipkart = FlipkartFactory.createFlipkart("blueDart");
+	public static void main(String[] args) throws Exception {
+		System.out.println("StrategyDPTest.main()");
+		Flipkart flipkart = FlipkartFactory.createFlipkart();
 		String result = flipkart.Shopping(new double[] {1000.00, 2000.00, 10000}, "Shirt", "Trouer", "Mobile");
 		System.out.println(result);
 	}
